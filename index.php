defdbc7559bf0cd9392d7cd6e5cf9802a1adabe2
Davit Margaryan <davit.margaryan16@gmail.com>
2025-12-13 11:33:54 +0400

    Dockerfile change

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
-define("BASE_URL", "/Teampoint/");
+define("BASE_URL", "/Teampoint");
 session_start();
 
 require_once __DIR__ . "/DV_Controllers.php";
