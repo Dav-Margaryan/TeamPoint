438c8cfb98f6de5bd9c92a473c5e0ad63b564e6c
Davit Margaryan <davit.margaryan16@gmail.com>
2025-12-13 11:12:39 +0400

    Dockerfile change

--- index.php
+++ index.php
@@ -1,17 +1,10 @@
 <?php
-echo "<pre>";
-print_r(scandir('/var/www/html'));
-echo file_exists("DV_Controllers.php") . "1";
-echo file_exists("/DV_Controllers.php") . "2";
-echo file_exists("․/DV_Controllers.php") . "3";
-echo "</pre>";
-
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 define("BASE_URL", "/Teampoint/");
 session_start();
-
-require_once "/DV_Controllers.php";
+echo file_exists(__DIR__ ."DV_Controllers.php") . __DIR__ ."DV_Controllers.php ka";
+require_once __DIR__ . "DV_Controllers.php";
 
 $router = new Router($_GET['url'] ?? 'Index/index');
 $router->dispatch();
