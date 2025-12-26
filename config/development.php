fe6a122ac621ec611c5e6917d61ad836e4fbb1a8
Davit Margaryan <davit.margaryan16@gmail.com>
2025-12-26 10:07:38 +0400

    multi changes

--- config/development.php
+++ config/development.php
@@ -3,14 +3,14 @@
 define("BASE_URL", "/");
 
 // DataBase configs
-define('HOST','shuttle.proxy.rlwy.net');
-define('DB_NAME','railway');
-define('USER','root');
+define('HOST',getenv('DB_HOST'));
+define('DB_NAME',getenv('DB_NAME'));
+define('USER',getenv('DB_USER'));
 define('PORT',10903);
 define('PASS',getenv('DB_PASS'));
 define('CHARSET','utf8mb4');
 
 // Email configs
-define("EMAIL_HOST",'smtp.gmail.com');
-define("EMAIL_USER",'app.teampoint@gmail.com');
-define("EMAIL_HASH",'fedi rjbh hdvh uqqh');
+define("EMAIL_HOST",getenv('SMTP_HOST'));
+define("EMAIL_USER",getenv('SMTP_USER'));
+define("EMAIL_HASH",getenv('SMTP_PASS'));
