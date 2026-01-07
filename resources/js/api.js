df5128d1aea45d88fe6a9653f58b24eec88589b8
Davit Margaryan <davit.margaryan16@gmail.com>
2026-01-07 19:09:15 +0400

    tinymce 8.3.1

--- resources/js/api.js
+++ resources/js/api.js
@@ -1,34 +1,17 @@
 $(document).ready(function() {
     if($("textarea.multi_lang_content").length) {
-
-        // tinymce.init({
-        //     selector: '#mytextarea',
-        //     // Add your configuration here
-        // });
-        //
         tinymce.init({
             selector: 'textarea.multi_lang_content',
             license_key: 'gpl',
-            toolbar_mode: 'wrap',
             plugins: [
-                // Core editing features
-                'anchor', 'autolink', 'charmap', 'codesample', 'emoticons', 'link', 'lists', 'media', 'searchreplace', 'table', 'visualblocks', 'wordcount',
-                // Your account includes a free trial of TinyMCE premium features
-
-                // Try the most popular premium features until Oct 14, 2025:
-                'checklist', 'mediaembed', 'casechange', 'formatpainter', 'pageembed', 'a11ychecker', 'tinymcespellchecker', 'permanentpen', 'powerpaste', 'advtable', 'advcode', 'advtemplate', 'ai', 'uploadcare', 'mentions', 'tinycomments', 'tableofcontents', 'footnotes', 'mergetags', 'autocorrect', 'typography', 'inlinecss', 'markdown','importword', 'exportword', 'exportpdf'
+                'accordion','anchor','autolink','autoresize','autosave',
+                'charmap','code','codesample','directionality','emoticons','fullscreen',
+                'help','image','importcss','insertdatetime','link','lists','media',
+                'nonbreaking','pagebreak','preview','quickbars','save','searchreplace',
+                'table','visualblocks','visualchars','wordcount'
             ],
             toolbar: 'undo redo | blocks fontfamily fontsize | bold italic underline strikethrough | link media table mergetags | addcomment showcomments | spellcheckdialog a11ycheck typography uploadcare | align lineheight | checklist numlist bullist indent outdent | emoticons charmap | removeformat',
-            tinycomments_mode: 'embedded',
-            tinycomments_author: 'Author name',
-            mergetags_list: [
-                {value: 'First.Name', title: 'First Name'},
-                {value: 'Email', title: 'Email'},
-            ],
-            // ai_request: (request, respondWith) => respondWith.string(() => Promise.reject('See docs to implement AI Assistant')),
-            // uploadcare_public_key: 'ad6009d5978d2650edf1',
         });
-
 
         const params = new URLSearchParams(window.location.search);
         const id = params.get("id");
