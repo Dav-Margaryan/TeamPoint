200059a623d326c809933c6529c93c594868ad85
Davit Margaryan <davit.margaryan16@gmail.com>
2026-01-14 14:11:10 +0400

    style change
    (cherry picked from commit 1361a79e4bf85dba3912a5ea1293e85a9ffe0a1a)

--- modules/views/layout.php
+++ modules/views/layout.php
@@ -21,6 +21,7 @@
         <title>TeamPoint</title>
         <!-- favicon PNG -->
         <link rel="icon" type="image/png" href="<?=BASE_URL?>resources/images/TeamPointIco.ico">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
 <!--        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">-->
 
         <?php require_once 'blocks/scripts.php'?>
