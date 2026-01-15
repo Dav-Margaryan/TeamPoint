16efb3d50865b7a034df46e37343d4d5f40b8584
Davit Margaryan <davit.margaryan16@gmail.com>
2026-01-15 17:53:30 +0400

    meta tags

--- modules/views/layout.php
+++ modules/views/layout.php
@@ -22,7 +22,7 @@
         <!-- favicon PNG -->
         <link rel="icon" type="image/png" href="<?=BASE_URL?>resources/images/TeamPointIco.ico">
         <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
-        <meta property="og:image" content="<?=BASE_URL?>resources/images/TeamPointOgImg.png">
+        <meta property="og:image" content="https://teampoint.onrender.com/resources/images/TeamPointOgImg.png">
         <meta property="og:image:width" content="1200">
         <meta property="og:image:height" content="630">
         <meta property="og:title" content="TeamPoint">
